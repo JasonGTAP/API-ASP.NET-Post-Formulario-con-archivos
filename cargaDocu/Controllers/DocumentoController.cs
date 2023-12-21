52757617f3a7f5c2a34a61dc55d9e4633491ce76
jason parraguez <jgg>
2023-12-20 21:11:24 -0300

    Agregar archivos de proyecto.

---
+++ cargaDocu/Controllers/DocumentoController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using cargaDocu.Models;
+
+namespace cargaDocu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentoController : ControllerBase
+    {
+
+        private readonly string _rutaServidor;
+        private readonly string _cadenaSql;
+
+
+
+        public DocumentoController(IConfiguration config) {
+            _rutaServidor = config.GetSection("Configuracion").GetSection("RutaServidor").Value;
+            _cadenaSql = config.GetConnectionString("CadenaSql");
+        
+        
+        }
+
+        [HttpPost]
+        [Route("subir")]
+
+
+        public IActionResult Subir([FromForm] Documento request) {
+
+
+            string rutaDocumento = Path.Combine(_rutaServidor, request.Archivo.FileName);
+
+            try
+            {
+                using(FileStream newFile = System.IO.File.Create(rutaDocumento))
+                {
+                    request.Archivo.CopyTo(newFile);
+                    newFile.Flush();   
+
+
+                }
+
+                using(var conexion = new SqlConnection(_cadenaSql))
+                {
+
+
+                    conexion.Open();
+
+                    var cmd = new SqlCommand("sp_guardar_documentos", conexion);
+                    cmd.Parameters.AddWithValue("nombreDocumento", request.NombreDocumento);
+                    cmd.Parameters.AddWithValue("ruta", rutaDocumento);
+                    cmd.Parameters.AddWithValue("idObra", request.IdObra);
+                    cmd.Parameters.AddWithValue("tipoDocumento", request.TipoDocumento);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();  
+                    
+                }
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Documento Guardado" });
+
+
+
+
+            }catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Documento no Guardado" + error });
+
+
+            }
+
+
+
+        
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ cargaDocu/Models/Documento.cs
@@ -0,0 +1,20 @@
+﻿namespace cargaDocu.Models
+{
+    public class Documento
+    {
+
+        
+        public int IdObra { get; set; }
+        public string NombreDocumento { get; set; }
+        public string Ruta { get; set; }
+        public string TipoDocumento { get; set; }
+        public IFormFile Archivo { get; set; }
+
+
+
+
+
+
+
+    }
+}
